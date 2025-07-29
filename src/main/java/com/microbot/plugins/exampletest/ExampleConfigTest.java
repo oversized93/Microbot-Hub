38e4c67c016bce0946f70e17efd10632a1bf7bdc
chsami <sami.chkhachkhi@gmail.com>
2025-07-30 00:00:31 +0200

    feat: update build configuration and add example plugin - Changed repository URL in build.gradle - Updated dependencies for microbot client - Added example plugin with configuration, overlay, and script - Enhanced README with project setup and usage instructions

---
+++ src/main/java/com/microbot/plugins/exampletest/ExampleConfigTest.java
@@ -0,0 +1,8 @@
+package microbot.plugins.exampletest;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("exampleTest")
+public interface ExampleConfigTest extends Config {
+}
