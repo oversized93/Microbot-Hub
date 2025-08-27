6df1153b2d133e9a37ad40bef607b6d73d33ae86
g-mason0 <19415334+g-mason0@users.noreply.github.com>
2025-08-27 11:00:33 -0400

    chore: use PluginConstants for Jad & TitheFarmingPlugin

--- src/main/java/net/runelite/client/plugins/microbot/tithefarming/TitheFarmingPlugin.java
+++ src/main/java/net/runelite/client/plugins/microbot/tithefarming/TitheFarmingPlugin.java
@@ -42,7 +42,7 @@ import java.util.regex.Pattern;
 @Slf4j
 public class TitheFarmingPlugin extends Plugin {
 
-    final static String version = "1.1.0";
+    final static String version = "1.1.1";
 
     @Inject
     public TitheFarmingConfig config;
