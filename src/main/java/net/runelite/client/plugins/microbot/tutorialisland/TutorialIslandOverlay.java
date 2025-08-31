0188e9a9629a6aa4245ee63651b8fe4ba5781227
Engin Karakurt <155079545+enginkarakurt@users.noreply.github.com>
2025-08-31 20:54:51 +0200

    feat(TutorialIsland): Migrate to Plugin Hub (#91)

---
+++ src/main/java/net/runelite/client/plugins/microbot/tutorialisland/TutorialIslandOverlay.java
@@ -0,0 +1,56 @@
+package net.runelite.client.plugins.microbot.tutorialisland;
+
+import net.runelite.client.plugins.microbot.Microbot;
+import net.runelite.client.ui.overlay.OverlayPanel;
+import net.runelite.client.ui.overlay.OverlayPosition;
+import net.runelite.client.ui.overlay.components.LineComponent;
+import net.runelite.client.ui.overlay.components.TitleComponent;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+public class TutorialIslandOverlay extends OverlayPanel {
+    
+    TutorialIslandPlugin plugin;
+    @Inject
+    TutorialIslandOverlay(TutorialIslandPlugin plugin) {
+        super(plugin);
+        this.plugin = plugin;
+        setPosition(OverlayPosition.TOP_LEFT);
+        setNaughty();
+    }
+    
+    @Override
+    public Dimension render(Graphics2D graphics) {
+        try {
+            panelComponent.setPreferredSize(new Dimension(200, 300));
+            panelComponent.getChildren().add(TitleComponent.builder()
+                    .text("Micro TutorialIsland V" + TutorialIslandPlugin.version)
+                    .color(Color.GREEN)
+                    .build());
+
+            panelComponent.getChildren().add(LineComponent.builder().build());
+
+            panelComponent.getChildren().add(LineComponent.builder()
+                    .left(Microbot.status)
+                    .build());
+            
+            if (plugin.isToggleDevOverlay()) {
+                if (TutorialIslandScript.status != null) {
+                    panelComponent.getChildren().add(LineComponent.builder()
+                            .left("State:")
+                            .right(TutorialIslandScript.status.toString())
+                            .build());
+                }
+
+                panelComponent.getChildren().add(LineComponent.builder()
+                        .left("Progress (281):")
+                        .right(Integer.toString(Microbot.getVarbitPlayerValue(281)))
+                        .build());
+            }
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+        return super.render(graphics);
+    }
+}
---
+++ src/main/java/net/runelite/client/plugins/microbot/tutorialisland/TutorialIslandPlugin.java
@@ -0,0 +1,105 @@
+package net.runelite.client.plugins.microbot.tutorialisland;
+
+import com.google.inject.Provides;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.config.ConfigManager;
+import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.plugins.microbot.PluginConstants;
+import net.runelite.client.ui.overlay.OverlayManager;
+
+import javax.inject.Inject;
+import java.awt.*;
+
+@PluginDescriptor(
+        name = PluginConstants.MOCROSOFT + "Tutorial Island",
+        description = "Microbot Tutorial Island plugin",
+        authors = { "Mocrosoft" },
+        version = TutorialIslandPlugin.version,
+        minClientVersion = "1.9.9.1",
+        tags = {"TutorialIsland", "microbot"},
+        iconUrl = "https://chsami.github.io/Microbot-Hub/TutorialIslandPlugin/assets/icon.png",
+        cardUrl = "https://chsami.github.io/Microbot-Hub/TutorialIslandPlugin/assets/card.png",
+        enabledByDefault = PluginConstants.DEFAULT_ENABLED,
+        isExternal = PluginConstants.IS_EXTERNAL
+)
+@Slf4j
+public class TutorialIslandPlugin extends Plugin {
+    public static final String version = "1.3";
+
+    @Getter
+    private boolean toggleMusic;
+    @Getter
+    private boolean toggleRoofs;
+    @Getter
+    private boolean toggleLevelUp;
+    @Getter
+    private boolean toggleShiftDrop;
+    @Getter
+    private boolean toggleDevOverlay;
+    
+    
+    @Inject
+    private TutorialIslandConfig config;
+    @Provides
+    TutorialIslandConfig provideConfig(ConfigManager configManager) {
+        return configManager.getConfig(TutorialIslandConfig.class);
+    }
+
+    @Inject
+    private OverlayManager overlayManager;
+    @Inject
+    private TutorialIslandOverlay tutorialIslandOverlay;
+
+    @Inject
+    TutorialIslandScript tutorialIslandScript;
+
+
+    @Override
+    protected void startUp() throws AWTException {
+        toggleMusic = config.toggleMusic();
+        toggleRoofs = config.toggleRoofs();
+        toggleLevelUp = config.toggleDisableLevelUp();
+        toggleShiftDrop = config.toggleShiftDrop();
+        toggleDevOverlay = config.toggleDevOverlay();
+        
+        if (overlayManager != null) {
+            overlayManager.add(tutorialIslandOverlay);
+        }
+        
+        tutorialIslandScript.run(config);
+    }
+
+    protected void shutDown() {
+        tutorialIslandScript.shutdown();
+        overlayManager.remove(tutorialIslandOverlay);
+    }
+
+    @Subscribe
+    public void onConfigChanged(final ConfigChanged event) {
+        if (!event.getGroup().equals(TutorialIslandConfig.configGroup)) return;
+        
+        if (event.getKey().equals(TutorialIslandConfig.toggleMusic)) {
+            toggleMusic = config.toggleMusic();
+        }
+        
+        if (event.getKey().equals(TutorialIslandConfig.toggleRoofs)) {
+            toggleRoofs = config.toggleRoofs();
+        }
+
+        if (event.getKey().equals(TutorialIslandConfig.toggleLevelUp)) {
+            toggleLevelUp = config.toggleDisableLevelUp();
+        }
+
+        if (event.getKey().equals(TutorialIslandConfig.toggleShiftDrop)) {
+            toggleShiftDrop = config.toggleShiftDrop();
+        }
+
+        if (event.getKey().equals(TutorialIslandConfig.toggleDevOverlay)) {
+            toggleDevOverlay = config.toggleDevOverlay();
+        }
+    }
+}
